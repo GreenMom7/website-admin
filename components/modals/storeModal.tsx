c3cafe8e3845b5862e6be37182b9c34729bc78e4
Akmal <akmalsabri73@gmail.com>
2025-07-15 11:54:56 +0800

    add store to database

--- components/modals/storeModal.tsx
+++ components/modals/storeModal.tsx
@@ -1,10 +1,14 @@
 "use client";
 
-import { useStoreModal } from "@/hooks/use-store-modal";
 import Modal from "../ui/modal";
+import axios from "axios";
+import { useState } from "react";
+import { useStoreModal } from "@/hooks/use-store-modal";
 import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
 import {
   Form,
   FormControl,
@@ -13,18 +17,13 @@ import {
   FormLabel,
   FormMessage,
 } from "../ui/form";
-import { Input } from "../ui/input";
-import { Button } from "../ui/button";
 
 const formSchema = z.object({
   name: z.string().min(1),
 });
 
-const onSubmit = async (values: z.infer<typeof formSchema>) => {
-  console.log(values);
-};
-
 export const StoreModal = () => {
+  const [loading, setLoading] = useState(false);
   const storeModal = useStoreModal();
 
   const form = useForm<z.infer<typeof formSchema>>({
@@ -33,6 +32,20 @@ export const StoreModal = () => {
       name: "",
     },
   });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setLoading(true);
+
+      const response = await axios.post("/api/stores", values);
+
+      // window.location.assign(`/${response.data.id}`);
+
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <Modal
