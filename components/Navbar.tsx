3032a93eec03dfc8b3fe8442467d848f6d78d05d
Akmal <akmalsabri73@gmail.com>
2025-07-17 23:40:07 +0800

    modify Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,26 @@
 import { UserButton } from "@clerk/nextjs";
 import { MainNav } from "./MainNav";
+import StoreSwitcher from "./StoreSwitcher";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { prisma } from "@/lib/db";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const stores = await prisma.store.findMany({
+    where: {
+      userId,
+    },
+  });
+
   return (
     <div className="border-b-2">
       <div className="flex h-16 items-center px-4">
-        <div>Store Switcher</div>
+        <StoreSwitcher items={stores} />
         <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
           <UserButton />
